6dbb5786de49a29821ff3720558a3aa474bc05ff
sakhnenkoff <matvey.sakhnenko@gmail.com>
2021-03-17 20:05:04 +0200

    created : added review slider , fixed minor bugs

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSwiper = new Swiper('.hotel-swiper-container', {
   // Optional parameters
 
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
 
   keyboard: {
@@ -14,8 +14,27 @@ const swiper = new Swiper('.swiper-container', {
     onlyInViewport: false,
   },
 
-
-
   effect: "coverflow",
+  
+});
+
+
+const reviewSwiper = new Swiper('.reviews-slider', {
+  // Optional parameters
+
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  
   
 });

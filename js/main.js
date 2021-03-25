dc4578b47efa6faf8e475e28af58aaf54655a51b
sakhnenkoff <matvey.sakhnenko@gmail.com>
2021-03-25 22:08:50 +0200

    created : form validation (not working for now)

--- js/main.js
+++ js/main.js
@@ -65,15 +65,39 @@ function openModal() {
    var modalDialog = $(".modal__dialog");
    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+   $('body').css('overflow','hidden');
 }
 
 function closeModal(event) {
+   $('body').css('overflow','auto');
+   if (event.key === "Escape" || event.type === 'click'){
    event.preventDefault();
    var modalOverlay = $(".modal__overlay");
    var modalDialog = $(".modal__dialog");
    modalOverlay.removeClass("modal__overlay--visible");
    modalDialog.removeClass("modal__dialog--visible");
 }
+event.preventDefault();
+}
+$(".form").each(function() {
+
+  $(this).validate({
+  errorClass: "invalid",
+  messages: {
+    name: {
+      required: "Please specify your name",
+      minlenght: "Name must be at least 2 charactes long",
+    },
+    email: {
+      required: "We need your email address to contact you",
+      email: "Your email address must be in the format of name@domain.com",
+    },
+    phone:{
+      required : "Phone number is required",
+    },
+  },
+});
+})
 
 
 });
